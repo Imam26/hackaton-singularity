ce076902e018e75e29b13b8fd943ec86fef91fa1
Azamat Alisher <azamat.alisher@jits-4-16.jits.ipa.dc>
2022-06-23 18:45:04 +0600

    add rating fragment

--- mobileapp/app/src/main/java/com/example/mobileapp/presentation/tournament/RatingFragment.kt
+++ mobileapp/app/src/main/java/com/example/mobileapp/presentation/tournament/RatingFragment.kt
@@ -2,13 +2,18 @@ package com.example.mobileapp.presentation.tournament
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.mobileapp.R
 
-class RatingFragment: Fragment(R.layout.fragment_rating) {
+class RatingFragment : Fragment(R.layout.fragment_rating) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        Toast.makeText(context, "rating fragment", Toast.LENGTH_SHORT).show()
+        //TODO: setup recyclerView
+
     }
+
 }
